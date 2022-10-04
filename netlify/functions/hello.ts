c45d12330d2bf956243e3d299bd0e143ef8978db
Matt Frisbie <msfrisbie@gmail.com>
2022-10-04 10:49:53 -0500

    test function

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,10 +1,21 @@
 import { Handler } from "@netlify/functions";
+import { createClient } from "redis";
 
 const handler: Handler = async (event, context) => {
-  // your server-side functionality
+  const url = `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_URL}:${process.env.REDIS_PORT}`;
+  const client = createClient({
+    url,
+  });
+
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  await client.connect();
+
+  await client.set("key", "value");
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: await client.get("key") }),
   };
 };
 
