945442a63351cd72f3ece26ad5cf1e7d6754b76e
Matt Frisbie <msfrisbie@gmail.com>
2022-11-19 10:37:09 -0600

    move out redux

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,14 @@ import Boilerplate from "@components/Boilerplate";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@styles/globals.scss";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "../redux/store.ts";
 
 config.autoAddCss = false;
 
 function Application({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <Boilerplate>
           <Component {...pageProps} />
         </Boilerplate>
-      </PersistGate>
-    </Provider>
   );
 }
 
--- pages/scan.js
+++ pages/scan.js
@@ -1,5 +1,8 @@
 import Header from "@components/Header";
 import ScanGantry from "@components/ScanGantry";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../redux/store.ts";
 
 export default function TagScanner() {
   return (
@@ -10,7 +13,11 @@ export default function TagScanner() {
         description="Scan Metrc tags with your phone or a bluetooth scanner, and download the list as a CSV"
       ></Header>
 
-      <ScanGantry></ScanGantry>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ScanGantry></ScanGantry>
+        </PersistGate>
+      </Provider>
     </>
   );
 }
